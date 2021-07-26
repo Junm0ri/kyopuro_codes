847ad7fa55dfe5742a542ba0eec0a30fb71ad599
Jun Moriuchi <79455149+Junm0ri@users.noreply.github.com>
2021-07-26 16:49:57 +0900

    Create problems.cpp

---
+++ problems.cpp
@@ -0,0 +1,17 @@
+// 問題類型化
+
+int Graph { //グラフ問題
+木 {
+    C - Folia https://atcoder.jp/contests/nomura2020/tasks/nomura2020_c
+    深さdにある葉の数が与えられる。それらを実現する二分木で最大の頂点数を答える。
+}
+ 
+}
+
+int String { //文字列問題
+変換系{
+    ABC110C String Transformation https://atcoder.jp/contests/abc110/tasks/abc110_c
+    文字列S,Tが与えられる。S内文字に対しa⇔bの変換を繰り返すことでTを作れるか
+}
+
+}
